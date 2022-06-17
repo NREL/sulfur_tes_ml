81a6de8432419c18dccc27e7c7e7b9d37216d8b7
Menear, Kevin <Kevin.Menear@nrel.gov>
2022-06-17 17:25:23 -0600

    Implementing base case and per-case models. Implementing recurrent for sulfur data

--- stesml/datatools_DEPRECATED.py
+++ stesml/datatools_DEPRECATED.py
@@ -12,6 +12,26 @@ def get_train_and_test_index(scenario_index):
     
     return train_index, test_index
 
+def load_data(scenario_index, selected_index, is_recurrent_test_data=False):
+    """ Load data from files in scenario_index with indices matching ones in selected_index"""
+    
+    df_arr = []
+    for f in scenario_index.loc[selected_index].filepath:
+        Tw = float(f.split("/")[-1].split("_")[1])
+        Ti = float(f.split("/")[-1].split("_")[2].replace(".csv", ""))
+
+        f_df = pd.read_csv(f, skiprows=12)
+        f_df["Tw"] = Tw
+        f_df["Ti"] = Ti
+        df_arr.append(f_df)
+    
+    if is_recurrent_test_data:
+        print(df_arr)
+        return df_arr
+    
+    combined_df = pd.concat(df_arr)
+    return combined_df
+
 def load_data_sulfur(scenario_index, selected_index, is_recurrent_test_data=False, per_case=False):
     """ Load data from files in scenario_index with indices matching ones in selected_index"""
     
@@ -45,7 +65,7 @@ def load_data_sulfur(scenario_index, selected_index, is_recurrent_test_data=Fals
 
 # transform a time series dataset into a supervised learning dataset
 # source: https://machinelearningmastery.com/random-forest-for-time-series-forecasting/
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True, per_case=False):
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     df = pd.DataFrame(data)
     cols = list()
     
@@ -66,6 +86,34 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True, per_case=False):
         
     return combined_df.values
 
+def get_train_data(scenario_index, train_index, is_recurrent=False, target='T'):
+    train_df = load_data(scenario_index, train_index)
+    if is_recurrent:
+        if target == 'T':
+            train = train_df[["Time", "Tw", "Ti", "Tc"]].to_numpy()
+        elif target == 'h':
+            train = train_df[["Time", "Tw", "Ti", "h"]].to_numpy()
+        else:
+            print('Target must be T or h.\n')
+            return None
+        train_shift = pd.DataFrame(series_to_supervised(train))
+        # Get rid of last prediction for each set, 
+        # because the next datapoint is the first datapoint of the next set
+        train_shift.drop(train_shift[train_shift[0] == 21.15].index, inplace = True)
+        train_shift = train_shift.to_numpy()
+        X_train = train_shift[:, 0:4]
+        y_train = train_shift[:, -1]
+    else:
+        X_train = train_df[["Time", "Tw", "Ti"]].to_numpy()
+        if target == 'T':
+            y_train = train_df[["Tc"]].to_numpy().reshape(-1,)
+        elif target == 'h':
+            y_train = train_df[["h"]].to_numpy().reshape(-1,)
+        else:
+            print('Target must be T or h.\n')
+            return None
+    return X_train, y_train
+
 def get_train_data_sulfur(scenario_index, train_index, test_index, is_recurrent=False, target='T',  per_case=False):
     train_df = load_data_sulfur(scenario_index, train_index)
     if is_recurrent:
@@ -77,14 +125,11 @@ def get_train_data_sulfur(scenario_index, train_index, test_index, is_recurrent=
             print('Target must be T or h.\n')
             return None
         train_shift = pd.DataFrame(series_to_supervised(train))
-        train_shift = pd.DataFrame(series_to_supervised(train))
         # Get rid of last prediction for each set, 
         # because the next datapoint is the first datapoint of the next set
-        train_shift.drop(abs(train_shift[train_shift[0] == 0].index - 1), inplace = True)
-        # Get rid of samples before 0.2 seconds to ensure all timesteps are equal to 0.1
-        train_shift.drop(train_shift[train_shift[0] < 0.2].index, inplace = True)
+        train_shift.drop(train_shift[train_shift[0] == 21.15].index, inplace = True)
         train_shift = train_shift.to_numpy()
-        X_train = train_shift[:, 0:5]
+        X_train = train_shift[:, 0:4]
         y_train = train_shift[:, -1]
     else:
         X_train = train_df[["flow-time", "Tw", "Ti", "Tx"]]
@@ -109,9 +154,43 @@ def get_train_data_sulfur(scenario_index, train_index, test_index, is_recurrent=
             y_train = y_train.to_numpy().reshape(-1,)
     return X_train, y_train
 
+def get_test_data(scenario_index, test_index, is_recurrent=False, target='T'):
+    if is_recurrent:
+        test_data = load_data(scenario_index, test_index, is_recurrent_test_data=True)
+        test_df_list = list()
+        for test_df in test_data:
+            if target == 'T':
+                test = test_df[["Time", "Tw", "Ti", "Tc"]].to_numpy()
+            elif target == 'h':
+                test = test_df[["Time", "Tw", "Ti", "h"]].to_numpy()
+            else:
+                print('Target must be T or h.\n')
+                return None
+            test_shift = series_to_supervised(test)
+            test_df_list.append(test_shift)
+        X_test = list()
+        y_test = list()
+        for test_shift in test_df_list:
+            X, Y = test_shift[:, 0:-5], test_shift[:, -1].tolist()
+            X_test.append(X)
+            y_test += Y
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
+    else:
+        test_df = load_data(scenario_index, test_index)
+        X_test = test_df[["Time", "Tw", "Ti"]].to_numpy()
+        if target == 'T':
+            y_test = test_df[["Tc"]].to_numpy().reshape(-1,)
+        elif target == 'h':
+            y_test = test_df[["h"]].to_numpy().reshape(-1,)
+        else:
+            print('Target must be T or h.\n')
+            return None
+        
+    return X_test, y_test
+
 def get_test_data_sulfur(scenario_index, test_index, is_recurrent=False, target='T'):
     if is_recurrent:
-        test_data = load_data_sulfur(scenario_index, test_index, is_recurrent_test_data=True)
         test_df_list = list()
         for test_df in test_data:
             if target == 'T':
@@ -126,7 +205,7 @@ def get_test_data_sulfur(scenario_index, test_index, is_recurrent=False, target=
         X_test = list()
         y_test = list()
         for test_shift in test_df_list:
-            X, Y = test_shift[:, 0:4], test_shift[:, -1].tolist()
+            X, Y = test_shift[:, 0:-5], test_shift[:, -1].tolist()
             X_test.append(X)
             y_test += Y
         X_test = np.array(X_test)
--- stesml/plot_tools_DEPRECATED.py
+++ stesml/plot_tools_DEPRECATED.py
@@ -25,6 +25,19 @@ def get_plot_data(y_hat, test_df, test_index, is_recurrent):
     
     return test_df
 
+def plot_test_results(test_df, model_type, target='T'):
+    for idx, grp in test_df.groupby(["Tw", "Ti"]):
+        if target == 'T':
+            ax = grp.plot(x="Time", y='Tc', c='DarkBlue', linewidth=2.5, label="Expected")
+        elif target == 'h':
+            ax = grp.plot(x="Time", y='h', c='DarkBlue', linewidth=2.5, label="Expected")
+        else:
+            print('Target must be T or h.\n')
+            return None
+        plot = grp.plot(x="Time", y='Tc_hat', c='DarkOrange', linewidth=2.5, label="Predicted ({model_type})".format(model_type=model_type), ax=ax)
+        plt.title('Tw = {Tw}  Ti = {Ti}'.format(Tw=idx[0], Ti=idx[1]))
+        plt.show()
+
 def plot_test_results_sulfur(test_df, model_type, target='T'):
     for idx, grp in test_df.groupby(["Tw", "Ti"]):
         if target == 'T':
@@ -40,13 +53,34 @@ def plot_test_results_sulfur(test_df, model_type, target='T'):
         plot = grp.plot(x="flow-time", y='Tc_hat', c='DarkOrange', linewidth=2.5, label="Predicted ({model_type})".format(model_type=model_type), ax=ax)
         plt.title('Tw = {Tw}  Ti = {Ti}'.format(Tw=idx[0], Ti=idx[1]))
         plt.show()
+        
+def plot_average_error(test_df):
+    ax = plt.figure(figsize=(10,5), dpi = 200).add_axes([0,0,1,1])
+    
+    count = 0
+    for idx, grp in test_df.groupby(["Tw", "Ti"]):
+        ax.plot(grp['Time'], abs(grp['Tc_hat'] - grp['Tc']), color='black', linewidth=1, alpha=0.5)
+        if count == 0:
+            avg_err = abs(grp['Tc_hat'] - grp['Tc'])
+        else:
+            avg_err += abs(grp['Tc_hat'] - grp['Tc'])
+        count += 1
+        
+    avg_err /= count
+    
+    ax.plot(grp['Time'], avg_err, color='r', linewidth=5)
+    #ax.set_ylim(bottom=0,top=20)
+    ax.set_xlim(left=0)
+    plt.title('Average Error at Time Step t')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Temperature (K)")
+    plt.show()
 
 def plot_average_error_sulfur(test_df):
     ax = plt.figure(figsize=(10,5), dpi = 200).add_axes([0,0,1,1])
     
     count = 0
     for idx, grp in test_df.groupby(["Tw", "Ti"]):
-        grp = grp.head(72000) # Trim dataset so all groups have same length
         ax.plot(grp['flow-time'], abs(grp['Tc_hat'] - grp['Tavg']), color='black', linewidth=1, alpha=0.5)
         if count == 0:
             avg_err = abs(grp['Tc_hat'] - grp['Tavg'])
@@ -62,6 +96,44 @@ def plot_average_error_sulfur(test_df):
     plt.title('Average Error at Time Step t')
     plt.xlabel("Time (s)")
     plt.ylabel("Temperature (K)")
+    plt.show()
+
+
+def plot_error_vs_T(test_df):
+    # This function plots the average difference of Tc-Tc_hat
+    # according to Tc. To make this meaningful, the x-axis is
+    # Tc, but each Tc is rounded down to the nearest integer.
+    # If this wasn't done, you would likely only end up with one
+    # datapoint per x-value, because Tc is a float and there are
+    # very few (if any) repeated Tc values.
+    
+    avg_temp_diff = {}
+
+    # Start out with a difference of 0 Kelvin
+    for i in range(300, 601):
+        avg_temp_diff[i] = [0,0]
+
+    # Get the difference, add the difference to the dictionary value
+    # with the key being the Temperature (Tc) rounded down to the nearest
+    # integer (floor). 
+    tc_hat_list = test_df['Tc_hat'].tolist()
+    for i, T in enumerate(test_df["Tc"]):
+        avg_temp_diff[math.floor(T)][0] += tc_hat_list[i] - T
+        avg_temp_diff[math.floor(T)][1] += 1
+
+    # Get the average temperature difference &
+    # create lists of x & y data for plotting
+    temperature = []
+    diff = []
+    for T, Tdiff_and_count in avg_temp_diff.items():
+        if Tdiff_and_count[1] > 0:
+            Tdiff_and_count[0] /= Tdiff_and_count[1]
+        temperature.append(T)
+        diff.append(Tdiff_and_count[0])
+
+    plt.scatter(temperature, diff)
+    plt.xlabel("Temperature (K)")
+    plt.ylabel("Average Error (K)")
     plt.show()
 
 def plot_progress_results(history, model_type, is_recurrent, metric):
