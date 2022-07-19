51e382f892cabeb126e75499db2aec0eacb62fb2
Menear, Kevin <Kevin.Menear@nrel.gov>
2022-07-18 19:24:59 -0600

    Removed unused imports.

--- stesml/data_tools.py
+++ stesml/data_tools.py
@@ -1,6 +1,5 @@
 import random
 import pandas as pd
-import numpy as np
 import glob
 import os
 
--- stesml/plot_tools.py
+++ stesml/plot_tools.py
@@ -1,5 +1,3 @@
-import math
-import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
